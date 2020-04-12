84f7e0bbf42007983a250c7d28500840ef10bcea
Morgante Pell <morgantep@google.com>
2020-04-11 23:40:22 -0400

    Add balls via matter.js

--- frontend/src/components/mousetrap/scene.js
+++ frontend/src/components/mousetrap/scene.js
@@ -1,22 +1,14 @@
 import PropTypes from "prop-types";
 import React from "react";
-import Matter, { Bodies } from "matter-js";
+import Matter, { Bodies, Composites, World } from "matter-js";
+import _ from "underscore";
 
 class Scene extends React.Component {
-    render () {
-        return (
-            Scene
-        )
+    state = {
+        balls: {}
     }
 
     componentDidMount() {
-        // var Engine = Matter.Engine,
-        //     Render = Matter.Render,
-        //     World = Matter.World,
-        //     Bodies = Matter.Bodies,
-        //     Mouse = Matter.Mouse,
-        //     MouseConstraint = Matter.MouseConstraint;
-        
         var engine = Matter.Engine.create({
             // positionIterations: 20
         });
@@ -31,19 +23,12 @@ class Scene extends React.Component {
             }
         });
 
-        var ballA = Bodies.circle(210, 100, 30, { restitution: 0.5 });
-        var ballB = Bodies.circle(110, 50, 30, { restitution: 0.5 });
-
-        Matter.World.add(engine.world, [
-          // walls
-          Bodies.rectangle(200, 0, 600, 50, { isStatic: true }),
-          Bodies.rectangle(200, 600, 600, 50, { isStatic: true }),
-          Bodies.rectangle(260, 300, 50, 600, { isStatic: true }),
-          Bodies.rectangle(0, 300, 50, 600, { isStatic: true })
+        World.add(engine.world, [
+            Bodies.rectangle(200, 150, 400, 20, { isStatic: true, angle: Math.PI * 0.06 }),
+            Bodies.rectangle(500, 350, 700, 20, { isStatic: true, angle: -Math.PI * 0.06 }),
+            Bodies.rectangle(340, 580, 700, 20, { isStatic: true, angle: Math.PI * 0.04 })
         ]);
     
-        Matter.World.add(engine.world, [ballA, ballB]);
-
         // add mouse control
         var mouse = Matter.Mouse.create(render.canvas);
         var mouseConstraint = Matter.MouseConstraint.create(engine, {
@@ -56,14 +41,34 @@ class Scene extends React.Component {
             }
         });
         Matter.World.add(engine.world, mouseConstraint);
-
-        Matter.Events.on(mouseConstraint, "mousedown", function(event) {
-          Matter.World.add(engine.world, Bodies.circle(150, 50, 30, { restitution: 0.7 }));
-        });
     
         Matter.Engine.run(engine);
     
         Matter.Render.run(render);
+
+        this.setState({
+            render: render
+        });
+    }
+
+    componentDidUpdate() {
+        console.log("updated", this.props.balls);
+
+        _.each(this.props.balls, (ball) => {
+            if (this.state.balls[ball.id] === undefined) {
+                const body = Bodies.circle(210, 100, 30, {
+                    friction: 0.00001,
+                    restitution: 0.5,
+                    density: 0.001 
+                });
+                this.state.balls[ball.id] = {
+                    ball: ball,
+                    body: body
+                };
+
+                Matter.World.add(this.state.render.engine.world, body);
+            }
+        });
     }
 
     render() {
